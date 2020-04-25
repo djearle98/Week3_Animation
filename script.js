4acff36c2bec984d561ac7b5d93b9041d4a6625c
David Earle <djearle98@icloud.com>
2020-04-25 02:36:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var allPresets = [];
 var obj = document.getElementById("obj");
+var currentPosition, startPosition, endPosition;
+var animationDuration = 3000; // in ms
+var animating = false;
+var interval;
+var frameNum = 0;
+var frameDuration = 10; //ms
+
 class Preset {
   constructor(width, height, left, top) {
     this.position = new Position(width, height, left, top)
-    this.pNum = allPresets.push(this);
-    var p = this;
+    this.pNum = allPresets.push(this) - 1;
     addButton(this);
   }
 }
@@ -27,23 +33,51 @@ function addButton(p) {
 }
 
 function animate(p) {
-  var startPosition = new Position(obj.style.width, obj.style.height, obj.style.left, obj.style.top);
-  var endPosition = p.position;
-
-  var currentPosition;
-  
-  //every X ms...
-  //update current position w/some algorithm
-  paint(endPosition);
+  startPosition = currentPosition;
+  endPosition = p.position;
+  interval = setInterval(frame, frameDuration);
+  frameNum = 0;
+  animating = true;
 }
 
-function paint(p){
-	obj.style.width = p.width + "px";
-  obj.style.height = p.height + "px";
-  obj.style.left = p.left + "px";
-  obj.style.top = p.top + "px";
+function frame(){
+  //Calculate position value
+  var percentDone = frameDuration*frameNum/animationDuration;
+  console.log("FRAME");
+  if (percentDone <= 1 && animating == true) {
+  	frameNum++;
+    currentPosition = linearAnimation(percentDone);
+	  paint();
+  } else {
+    clearInterval(interval);
+    animating = false;
+  }
 }
 
-var p1 = new Preset(100, 200, 300, 10);
+function paint(){
+	//update the canvas
+	obj.style.width = currentPosition.width + "px";
+  obj.style.height = currentPosition.height + "px";
+  obj.style.left = currentPosition.left + "px";
+  obj.style.top = currentPosition.top + "px";
+}
+
+function linearAnimation(percentDone) {
+
+var invPercentDone = 1-percentDone;
+  /* currentPosition.width = startPosition.width*invPercentDone + endPosition.width*percentDone;
+  currentPosition.height = startPosition.height*invPercentDone + endPosition.height*percentDone;
+  currentPosition.left = startPosition.left*invPercentDone + endPosition.left*percentDone;
+  */
+  var width = startPosition.width*invPercentDone + endPosition.width*percentDone;
+  var height = startPosition.height*invPercentDone + endPosition.height*percentDone;
+  var left = startPosition.left*invPercentDone + endPosition.left*percentDone;
+  var top = startPosition.top*invPercentDone + endPosition.top*percentDone;
+  return new Position(width, height, left, top);
+
+}
+
+var p1 = new Preset(100, 200, 100, 50);
 var p2 = new Preset(50, 50, 300, 10);
-animate(p1);
+currentPosition = p1.position;
+paint();
