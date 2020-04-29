f9b0703c394749a9415df9f944887c2f3960c988
David Earle <djearle98@icloud.com>
2020-04-28 22:15:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,94 +3,138 @@ var obj = document.getElementById("obj");
 var presetButtons = [];
 
 /*CLASSES*/
-//TODO: Breakout ease-in/ease-out envelope and add this setting to constructor
-//TODO: Make animation settings editable via setters
-//TODO: access CSS like this: 
-	//element.style.setProperty("background-color", "red");
 class Animation {
-  constructor(obj, animType, frameDur, animDur) {
+  constructor(obj, animAlgorithm, ease, frameDur, animDur) {
     this.obj = obj;
-    if (true){  //animType == "Linear"
-    	this.animAlgorithm = this.linear;
-    }
+    this.setAnimAlgorithm(animAlgorithm);
     this.frameDur = frameDur; //ms
     this.animDur = animDur; //ms
-    
+    this.ease = ease;
+
     this.animating = false;
     this.frameNum = 0;
-    this.clockTrigger = null;
+    this.clock = null;
     this.startData = [];
     this.curData = [];
     this.endData = [];
     this.percentDone = 0;
-
+    this.progress = 0;
+    this.parameters = [];
   }
+  
+  addParameter(name, unit){
+  	/*var p = {
+    	name: name,
+      unit: unit,
+    };
+    */
+  	this.parameters.push({
+    	name: name,
+      unit: unit,
+      display: function(value){
+      	if(unit=="rgb"){
+        	return "rgb("+value+")";
+        } else {
+        	return""+value+unit;
+        }
+      }
+    });
+  }
+  
+  setAnimAlgorithm(algorithm) {
+    switch (algorithm.toLowerCase()) {
+      case "linear":
+        this.animAlgorithm = this.linear;
+        break;
+      default:
+        this.animAlgorithm = this.linear;
+    }
+  }
+
+  get progress() {
+    if (this.ease == true) {
+      return 1 - (Math.cos(this.percentDone * Math.PI) + 1) / 2;
+    } else {
+      return this.percentDone;
+    }
+  }
+  set progress(value) {}
   anim(endData) {
-  	this.endData = endData;
+    this.endData = endData;
     this.startData = this.curData;
     this.frameNum = 0;
-		cancelAnimationFrame(this.clockTrigger);
+    cancelAnimationFrame(this.clock);
 
-		this.calcFrame();
-	}
-  calcFrame(){
-    this.percentDone = (this.frameDur*this.frameNum)/this.animDur;
+    this.calcFrame();
+  }
+  calcFrame() {
+    this.percentDone = (this.frameDur * this.frameNum) / this.animDur;
     if (this.percentDone <= 1) {
-    	this.clockTrigger = requestAnimationFrame(animationHandler);
       this.frameNum++;
       this.curData = this.animAlgorithm();
       this.paint();
+      this.clock = requestAnimationFrame(animationHandler);
     }
   }
-	linear() {
-  	var progress = 1-(Math.cos(this.percentDone*Math.PI)+1)/2;
-    var invProgress = 1-progress;
+  linear() {
+    var progress = this.progress;
+    var invProgress = 1 - progress;
     var result = [];
-    for(var i=0; i<this.endData.length; i++) { 
-    	result.push(this.startData[i]*invProgress + this.endData[i]*progress);
+    for (var i = 0; i < this.endData.length; i++) {
+    	if(Array.isArray(this.endData[i])){
+      	var subResult = [];
+      	for (var j = 0; j < this.endData[i].length; j++) {
+          subResult.push(this.startData[i][j] * invProgress + this.endData[i][j] * progress);
+        }
+				result.push(subResult);
+      } else {
+	      result.push(this.startData[i] * invProgress + this.endData[i] * progress);
+      }
     }
     return result;
   }
-  paint(){
+  paint() {
     //update the object
-    this.obj.style.width = this.curData[0] + "px";
-    this.obj.style.height = this.curData[1] + "px";
-    this.obj.style.left = this.curData[2] + "px";
-    this.obj.style.top = this.curData[3] + "px";
+		for(var i=0; i<this.parameters.length; i++){
+    	this.obj.style.setProperty(this.parameters[i].name, this.parameters[i].display(this.curData[i]));
+    }
   }
-  init(data){
-  	this.curData = data;
+  init(data) {
+    this.curData = data;
     this.paint();
   }
 }
 
 /*FUNCTIONS*/
 function addPreset(data) {
-  var i = presetButtons.push(document.createElement("button"))-1;
+  var i = presetButtons.push(document.createElement("button")) - 1;
   var button = presetButtons[i];
   button.setAttribute("type", "button");
-  button.addEventListener("click", function(){onclickHandler(data);}, false);
-  var text = document.createTextNode("Preset: ["+data+"]");
+  button.addEventListener("click", function() {
+    onclickHandler(data);
+  }, false);
+  var text = document.createTextNode("Preset: [" + data + "]");
   button.appendChild(text);
   document.body.appendChild(button);
 }
 function onclickHandler(data) {
-	animFrameSize.anim(data)
+  animFrameSize.anim(data)
 }
-function animationHandler(){
-	animFrameSize.calcFrame();
+function animationHandler() {
+  animFrameSize.calcFrame();
   console.log("FRAME");
 }
 
 /*EXECUTION*/
-var animFrameSize = new Animation(obj, "Linear", 10, 800);
-/* TODO: Add functionality for addParameters in the class w/ customizable units. 
-animFrameSize.addParameter("width", "px");
-animFrameSize.addParameter("height", "px");
-animFrameSize.addParameter("left", "px");
-animFrameSize.addParameter("top", "px");
-*/
-animFrameSize.init([482, 482, 8, 8]);
-addPreset([482, 482, 8, 8]);
-addPreset([450, 450, 25, 25]);
-addPreset([200, 450, 150, 25]);
+var animFrameSize = new Animation(obj, "Linear", true, 10, 800);
+animFrameSize.addParameter("width", "%");
+animFrameSize.addParameter("height", "%");
+animFrameSize.addParameter("left", "%");
+animFrameSize.addParameter("top", "%");
+animFrameSize.addParameter("border-radius", "px");
+animFrameSize.addParameter("background-color","rgb");
+
+animFrameSize.init([95, 95, 2.5, 2.5, 0, [255,0,0]]);
+addPreset([95, 95, 2.5, 2.5, 0, [255,0,0]]);
+addPreset([90, 90, 5, 5, 15, [0,255, 255]]);
+addPreset([50, 90, 25, 5, 50, [128,128,128]]);
