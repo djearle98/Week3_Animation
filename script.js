5eb8ae49667d52096dd1ff890f4f78f9f550e5f0
David Earle <djearle98@icloud.com>
2020-05-01 02:55:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-var obj = document.getElementById("obj");
-var parameters = ["width", "height", "left", "top", "border-radius", "background-color"];
-var presets = [];
+var editButton = document.getElementById("edit");
+editButton.onclick = editClicked;
 
-function addPreset(data) {
-  var i = presets.push(document.createElement("button")) - 1;
-  var button = presets[i];
-  button.setAttribute("type", "button");
-  button.addEventListener("click", function() {
-    changeCSS(data);
-  }, false);
-  var text = document.createTextNode("Preset: [" + data + "]");
-  button.appendChild(text);
-  document.body.appendChild(button);
+let root = document.documentElement;
+
+function editClicked(){
+  root.style.setProperty('--canvas-width', "50vw");
+  root.style.setProperty('--node-size', "200px");
 }
-function changeCSS(data){
-	for (var i=0; i<parameters.length; i++){
-  	obj.style.setProperty(parameters[i], data[i])
-  }
-}
-/*EXECUTION*/
-addPreset(["95%", "95%", "2.5%", "2.5%", "50%", "#FF99C8"]);
-addPreset(["90%", "90%", "5%", "5%", "15px", "#A288E3"]);
-addPreset(["50%", "90%", "25%", "5%", "50px", "#75DDDD"]);
-document.getElementsByTagName("button")[0].click();
